1720fae3da18bd15bb09c73e70c4491ae0b20321
Andrey529 <andr02eyes@gmail.com>
2023-02-19 15:45:19 +0300

    changed usage example in CurlCppProgram

--- CurlCppProgram/src/main.cpp
+++ CurlCppProgram/src/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <curl/curl.h>
 
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    static_cast<std::string *>(userdata)->append(static_cast<char *>(ptr), size * nmemb);
+    return size * nmemb;
+}
+
 int main(int argc, char **argv) {
-    std::cout << "Hello world" << std::endl;
     CURL *curl = curl_easy_init();
+
     if(curl) {
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
-        res = curl_easy_perform(curl);
+        std::string response;
+        std::string url("https://example.com");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        /* ask libcurl to show us the verbose output */
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+
+        /* Perform the request */
+        CURLcode res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         std::cout << res << std::endl;
+        std::cout << response << std::endl;
     }
 }
